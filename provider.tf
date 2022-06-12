d15ef56a604cf746a7ef408f95e516c3b1ccbcfe
Rohit chandra chaubey <86163674+Rohitchaubey12@users.noreply.github.com>
2022-06-12 12:09:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-northeast-1"
-  access_key = "AKIA42TA2MLWUDQ6GENU"
-  secret_key = "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"
+  access_key = "A
+  secret_key = "
 }
